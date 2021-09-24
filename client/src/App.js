8eaf1352e167fcdd9849292dba394a942a610d57
ichbinjaeger <ichbinjaeger@users.noreply.github.com>
2021-09-25 01:16:04 +0200

    add navigation, custom theme, styling to views

--- client/src/App.js
+++ client/src/App.js
@@ -1,32 +1,56 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 import logo from './logo.svg';
 import './App.scss';
 
 // Views
-import LoginView from "./components/views/LoginView";
+import LoginView from './components/views/LoginView';
 import RegisterView from './components/views/RegisterView';
 import DiscsView from './components/views/DiscsView';
 import ScorecardsView from './components/views/Scorecards';
 
 const App = () => {
-    return (
-        <Router>
-            <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                </header>
-                <div>
-                    <Switch>
-                        <Route exact path='/login' component={LoginView} />
-                        <Route exact path='/register' component={RegisterView} />
-                        <Route exact path='/discs' component={DiscsView} />
-                        <Route exact path='/scorecards' component={ScorecardsView} />
-                    </Switch>
-                </div>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <nav>
+          <ul className="p-0">
+            <li>
+              <a className="text-white" href="/login">
+                Login
+              </a>
+            </li>
+            <li>
+              <a className="text-white" href="/register">
+                Register
+              </a>
+            </li>
+            <li>
+              <a className="text-white" href="/discs">
+                Discs
+              </a>
+            </li>
+            <li>
+              <a className="text-white" href="/scorecards">
+                Scorecards
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <div>
+          <Switch>
+            <Route exact path="/login" component={LoginView} />
+            <Route exact path="/register" component={RegisterView} />
+            <Route exact path="/discs" component={DiscsView} />
+            <Route exact path="/scorecards" component={ScorecardsView} />
+          </Switch>
+        </div>
+      </div>
+    </Router>
+  );
 };
 
 export default App;
--- client/src/components/views/RegisterView.js
+++ client/src/components/views/RegisterView.js
@@ -1,31 +1,60 @@
 import React from 'react';
 
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
+import './RegisterView.scss';
+
 const RegisterView = () => {
-    return (
-        <div className="register-view">
-            <h1>Register</h1>
-            <form action="" method="post">
-                <h2>Enter your credentials below</h2>
-                <div className="form-group">
-                    <label htmlFor="username">Username</label>
-                    <input type="text" required id="username" placeholder="Enter username" />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="email">Email</label>
-                    <input type="text" required id="email" placeholder="Enter email" />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="password">Enter a password</label>
-                    <input type="password" required id="password" placeholder="Enter password" />
-                </div>
-                <div className="form-group">
-                    <label htmlFor="password-confirmation">Re-enter password</label>
-                    <input type="password" required id="password-confirmation" placeholder="Re-enter password" />
-                </div>
-                <button type="submit">Register</button>
-            </form>
+  return (
+    <div className="register-view">
+      <h1>Register</h1>
+      <Form>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Text className="text-muted">We'll never share your email.</Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Label>Re-enter password</Form.Label>
+          <Form.Control type="password" placeholder="Re-enter password" />
+        </Form.Group>
+
+        <Button variant="primary" className="text-white" type="submit">
+          Register
+        </Button>
+        <a href="/login" className="link-light" type="submit">
+          Already registered? Login here.
+        </a>
+      </Form>
+      {/* <form action="" method="post">
+        <h2>Enter your credentials below</h2>
+        <div className="form-group">
+          <label htmlFor="username">Username</label>
+          <input type="text" required id="username" placeholder="Enter username" />
         </div>
-    );
+        <div className="form-group">
+          <label htmlFor="email">Email</label>
+          <input type="text" required id="email" placeholder="Enter email" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Enter a password</label>
+          <input type="password" required id="password" placeholder="Enter password" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="password-confirmation">Re-enter password</label>
+          <input type="password" required id="password-confirmation" placeholder="Re-enter password" />
+        </div>
+        <button type="submit">Register</button>
+      </form> */}
+    </div>
+  );
 };
 
 export default RegisterView;

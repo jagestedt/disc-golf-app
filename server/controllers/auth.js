f1911184117bacaf124dc2a4e3e0979043bce29a
ichbinjaeger <ichbinjaeger@users.noreply.github.com>
2021-09-28 18:53:25 +0200

    add login logic, ECONNREFUSED

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -22,8 +22,30 @@ exports.register = async (req, res, next) => {
   }
 };
 
-exports.login = (req, res, next) => {
-  res.send('login route');
+exports.login = async (req, res, next) => {
+  const {email, password} = req.body;
+
+  if (!email || !password) {
+    res.status(400).json({success: false, error: 'Enter email and password'});
+
+    try {
+      const user = await User.findOne({email}).select('+password');
+
+      if (!user) {
+        res.status(404).json({success: false, error: 'No user with that email, enter correct information'});
+      }
+
+      const isMatch = await user.matchPasswords(password);
+
+      if (!isMatch) {
+        res.status(404).json({success: false, error: 'Wrong login credentials'});
+
+        res.status(200).json({success: true, token: '535432f54ga3'});
+      }
+    } catch (error) {
+      res.status(500).json({success: false, error: error.message});
+    }
+  }
 };
 
 exports.forgotPassWord = (req, res, next) => {

7cb9baf92d9013e8075e084156d56362a9d3dfb2
ichbinjaeger <ichbinjaeger@users.noreply.github.com>
2021-10-01 16:20:29 +0200

    remove redundant id attributes, remove old code

--- client/src/components/views/LoginView.js
+++ client/src/components/views/LoginView.js
@@ -48,12 +48,12 @@ const LoginView = ({history}) => {
       <Form onSubmit={loginHandler}>
         <Form.Group className="mb-3" controlId="formBasicEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" required id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+          <Form.Control type="email" placeholder="Enter email" required value={email} onChange={(e) => setEmail(e.target.value)} />
         </Form.Group>
 
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" required id="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <Form.Control type="password" placeholder="Password" required value={password} onChange={(e) => setPassword(e.target.value)} />
         </Form.Group>
 
         <Button variant="primary" className="text-white" type="submit">
@@ -64,26 +64,6 @@ const LoginView = ({history}) => {
           No account? Register here.
         </a>
       </Form>
-      {/* <form action="" method="post">
-        <h2>Enter your credentials below</h2>
-        <div className="form-group">
-          <label htmlFor="username">Username</label>
-          <input type="text" required id="username" placeholder="Enter username" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="email">Email</label>
-          <input type="text" required id="email" placeholder="Enter email" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password">Enter a password</label>
-          <input type="password" required id="password" placeholder="Enter password" />
-        </div>
-        <div className="form-group">
-          <label htmlFor="password-confirmation">Re-enter password</label>
-          <input type="password" required id="password-confirmation" placeholder="Re-enter password" />
-        </div>
-        <button type="submit">Register</button>
-      </form> */}
     </div>
   );
 };

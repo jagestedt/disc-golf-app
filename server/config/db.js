55dcc3bba06169018a8b7858477d830a223a6295
ichbinjaeger <ichbinjaeger@users.noreply.github.com>
2021-10-10 21:12:47 +0200

    change env variable

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const connectDb = async () => {
-  await mongoose.connect(process.env.MONGODB_URL, {
+  await mongoose.connect(process.env.MONGO_DB_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });

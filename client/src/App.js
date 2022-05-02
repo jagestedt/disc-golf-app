5b0b6f71735d6623bf62c692622ea96ae61552a0
Samuel Jägestedt <ichbinjaeger@gmail.com>
2022-05-02 11:18:15 +0200

    add bootstrap navbar, cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,65 +1,53 @@
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
 
 // Routing
-import ProtectedRoute from './components/routing/ProtectedRoute';
+import ProtectedRoute from "./components/routing/ProtectedRoute"
 
-import logo from './logo.svg';
-import './App.scss';
+import "./App.scss"
+
+import { Nav, Navbar } from "react-bootstrap"
 
 // Views
-import ProtectedView from './components/views/ProtectedView';
-import LoginView from './components/views/Users/LoginView';
-import RegisterView from './components/views/Users/RegisterView';
-import DiscsView from './components/views/Discs/DiscsView';
-import ScorecardsView from './components/views/Scorecards';
-import UsersView from './components/views/Users/UsersView';
-// import DiscsListView from './components/views/Discs/DiscsListView';
+import LoginView from "./components/views/Users/LoginView"
+import RegisterView from "./components/views/Users/RegisterView"
+import DiscsView from "./components/views/Discs/DiscsView"
+import ScorecardsView from "./components/views/Scorecards"
+import UsersView from "./components/views/Users/UsersView"
 
 const App = () => {
   return (
     <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+        <header>
+          <Navbar expand="sm">
+            <Navbar.Brand href="/">dga-wip</Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="me-auto">
+                <Nav.Link href="/login">Login</Nav.Link>
+                <Nav.Link href="/register">Register</Nav.Link>
+                <Nav.Link href="/discs">Discs</Nav.Link>
+                <Nav.Link href="/users">Users</Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
+          </Navbar>
         </header>
-        <nav>
-          <ul className="p-0">
-            <li>
-              <a className="text-primary" href="/login">
-                Login
-              </a>
-            </li>
-            <li>
-              <a className="text-primary" href="/register">
-                Register
-              </a>
-            </li>
-            <li>
-              <a className="text-primary" href="/discs">
-                Discs
-              </a>
-            </li>
-            <li>
-              <a className="text-primary" href="/users">
-                Users
-              </a>
-            </li>
-          </ul>
-        </nav>
         <div>
           <Switch>
-            <ProtectedRoute exact path="/" component={ProtectedView} />
             <Route exact path="/login" component={LoginView} />
             <Route exact path="/register" component={RegisterView} />
             <ProtectedRoute exact path="/discs" component={DiscsView} />
-            <ProtectedRoute exact path="/scorecards" component={ScorecardsView} />
+            <ProtectedRoute
+              exact
+              path="/scorecards"
+              component={ScorecardsView}
+            />
             <ProtectedRoute exact path="/users" component={UsersView} />
-            {/* <ProtectedRoute exact path="/discs-list" component={DiscsListView} /> */}
           </Switch>
         </div>
       </div>
     </Router>
-  );
-};
+  )
+}
 
-export default App;
+export default App
